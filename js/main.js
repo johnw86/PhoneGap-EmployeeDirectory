62a0a81ec1369d581ed0c3df69daccb1621f4f82
John Walker <john@prodo.com>
2013-03-11 16:04:33 +0000

    complete part 8 of tutorial

--- js/main.js
+++ js/main.js
@@ -8,28 +8,37 @@ var app = {
         }
     },
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+    registerEvents: function () {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function (event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function (event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function (event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function (event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     },
 
-    initialize: function() {
+    initialize: function () {
         var self = this;
-        this.store = new WebSqlStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+        this.store = new WebSqlStore(function () {
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+        this.registerEvents();
+
     }
-
-
 
 };
 
